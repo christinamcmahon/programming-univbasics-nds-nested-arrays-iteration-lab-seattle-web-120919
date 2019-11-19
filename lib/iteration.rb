d30df546962aebde6e5d1793b166a851753c0ba8
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-19 23:00:36 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -23,9 +23,9 @@ def find_greater_pair(src)
   row_index = 0
   while row_index < src.count do
     if src[row_index][0] >= src[row_index][1]
-      result.push(src[row_index][element_index][0])
+      result.push(src[row_index][0])
     else
-      result.push(src[row_index][element_index][1])
+      result.push(src[row_index][1])
     end
     row_index += 1
   end
